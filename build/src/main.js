15b103501780f3ba6b48a01cb8facdbb0f304c6b
Dogu Technologies <admin@dogutech.io>
2023-07-22 10:02:36 +0000

    🚀 @dogu-actions/checkout@production

--- build/src/main.js
+++ build/src/main.js
@@ -1,13 +1,9 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
 Object.defineProperty(exports, "__esModule", { value: true });
 const action_kit_1 = require("@dogu-tech/action-kit");
 const child_process_1 = require("child_process");
-const fs_1 = __importDefault(require("fs"));
 action_kit_1.ActionKit.run(async ({ options, logger, input, deviceHostClient, consoleActionClient, deviceClient }) => {
-    const { DOGU_DEVICE_WORKSPACE_PATH, DOGU_PROJECT_ID } = options;
+    const { DOGU_ROUTINE_WORKSPACE_PATH, DOGU_PROJECT_ID } = options;
     const clean = input.get('clean');
     const branchOrTag = input.get('branchOrTag');
     const postCommand = input.get('postCommand');
@@ -16,10 +12,8 @@ action_kit_1.ActionKit.run(async ({ options, logger, input, deviceHostClient, co
         logger.info('Using local user project...');
     }
     else {
-        await (0, action_kit_1.checkoutProject)(logger, consoleActionClient, deviceHostClient, DOGU_DEVICE_WORKSPACE_PATH, DOGU_PROJECT_ID, branchOrTag, clean);
-        const deviceProjectWorkspacePath = action_kit_1.HostPaths.deviceProjectWorkspacePath(DOGU_DEVICE_WORKSPACE_PATH, DOGU_PROJECT_ID);
-        await fs_1.default.promises.mkdir(deviceProjectWorkspacePath, { recursive: true });
-        const deviceProjectGitPath = action_kit_1.HostPaths.deviceProjectGitPath(deviceProjectWorkspacePath);
+        await (0, action_kit_1.checkoutProject)(logger, consoleActionClient, deviceHostClient, DOGU_ROUTINE_WORKSPACE_PATH, branchOrTag, clean);
+        const workspacePath = DOGU_ROUTINE_WORKSPACE_PATH;
         if (postCommand) {
             logger.info('Running post command...');
             const command = process.platform === 'win32' ? process.env.COMSPEC || 'cmd.exe' : process.env.SHELL || '/bin/sh';
@@ -28,7 +22,7 @@ action_kit_1.ActionKit.run(async ({ options, logger, input, deviceHostClient, co
             logger.info('Running command', { command, args });
             const result = (0, child_process_1.spawnSync)(command, args, {
                 stdio: 'inherit',
-                cwd: deviceProjectGitPath,
+                cwd: workspacePath,
                 env: (0, action_kit_1.newCleanNodeEnv)(),
             });
             logger.verbose?.('Command result', { result });
