91123f1b59ccbc786aaaaf08c7fb2e29946462e6
Dogu Technologies <admin@dogutech.io>
2023-08-19 11:23:59 +0000

    🚀 @dogu-actions/checkout@production

--- build/src/main.js
+++ build/src/main.js
@@ -1,19 +1,28 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 const action_kit_1 = require("@dogu-tech/action-kit");
 const child_process_1 = require("child_process");
+const path_1 = __importDefault(require("path"));
 action_kit_1.ActionKit.run(async ({ options, logger, input, deviceHostClient, consoleActionClient, deviceClient }) => {
     const { DOGU_ROUTINE_WORKSPACE_PATH, DOGU_PROJECT_ID } = options;
     const clean = input.get('clean');
     const branchOrTag = input.get('branchOrTag');
     const postCommand = input.get('postCommand');
+    const checkoutPath = input.get('checkoutPath');
+    const checkoutUrl = input.get('checkoutUrl');
+    logger.info('resolve checkout path... from', { DOGU_ROUTINE_WORKSPACE_PATH, checkoutPath });
+    const resolvedCheckoutPath = path_1.default.resolve(DOGU_ROUTINE_WORKSPACE_PATH, checkoutPath);
+    logger.info('resolved checkout path', { resolvedCheckoutPath });
     const optionsConfig = await action_kit_1.OptionsConfig.load();
     if (optionsConfig.get('localUserProject.use', false)) {
         logger.info('Using local user project...');
     }
     else {
-        await (0, action_kit_1.checkoutProject)(logger, consoleActionClient, deviceHostClient, DOGU_ROUTINE_WORKSPACE_PATH, branchOrTag, clean);
-        const workspacePath = DOGU_ROUTINE_WORKSPACE_PATH;
+        await (0, action_kit_1.checkoutProject)(logger, consoleActionClient, deviceHostClient, resolvedCheckoutPath, branchOrTag, clean, checkoutUrl);
+        const workspacePath = resolvedCheckoutPath;
         if (postCommand) {
             logger.info('Running post command...');
             const command = process.platform === 'win32' ? process.env.COMSPEC || 'cmd.exe' : process.env.SHELL || '/bin/sh';
