1d66f26c81230410a2881ae946d2e9349ffd95fc
Dogu Technologies <admin@dogutech.io>
2023-09-02 11:35:20 +0000

    🚀 @dogu-actions/checkout@production

--- build/src/main.js
+++ build/src/main.js
@@ -6,8 +6,8 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const action_kit_1 = require("@dogu-tech/action-kit");
 const child_process_1 = require("child_process");
 const path_1 = __importDefault(require("path"));
-action_kit_1.ActionKit.run(async ({ options, logger, input, deviceHostClient, consoleActionClient, deviceClient }) => {
-    const { DOGU_ROUTINE_WORKSPACE_PATH, DOGU_PROJECT_ID } = options;
+action_kit_1.ActionKit.run(async ({ options, logger, input, deviceHostClient, consoleActionClient }) => {
+    const { DOGU_ROUTINE_WORKSPACE_PATH } = options;
     const clean = input.get('clean');
     const branchOrTag = input.get('branchOrTag');
     const postCommand = input.get('postCommand');
@@ -16,28 +16,22 @@ action_kit_1.ActionKit.run(async ({ options, logger, input, deviceHostClient, co
     logger.info('resolve checkout path... from', { DOGU_ROUTINE_WORKSPACE_PATH, checkoutPath });
     const resolvedCheckoutPath = path_1.default.resolve(DOGU_ROUTINE_WORKSPACE_PATH, checkoutPath);
     logger.info('resolved checkout path', { resolvedCheckoutPath });
-    const optionsConfig = await action_kit_1.OptionsConfig.load();
-    if (optionsConfig.get('localUserProject.use', false)) {
-        logger.info('Using local user project...');
-    }
-    else {
-        await (0, action_kit_1.checkoutProject)(logger, consoleActionClient, deviceHostClient, resolvedCheckoutPath, branchOrTag, clean, checkoutUrl);
-        const workspacePath = resolvedCheckoutPath;
-        if (postCommand) {
-            logger.info('Running post command...');
-            const command = process.platform === 'win32' ? process.env.COMSPEC || 'cmd.exe' : process.env.SHELL || '/bin/sh';
-            const args = process.platform === 'win32' ? ['/d', '/s', '/c'] : ['-c'];
-            args.push(postCommand);
-            logger.info('Running command', { command, args });
-            const result = (0, child_process_1.spawnSync)(command, args, {
-                stdio: 'inherit',
-                cwd: workspacePath,
-                env: (0, action_kit_1.newCleanNodeEnv)(),
-            });
-            logger.verbose?.('Command result', { result });
-            if (result.status !== 0) {
-                throw new Error(`Post command failed with status ${result.status}`);
-            }
+    await (0, action_kit_1.checkoutProject)(logger, consoleActionClient, deviceHostClient, resolvedCheckoutPath, branchOrTag, clean, checkoutUrl);
+    const workspacePath = resolvedCheckoutPath;
+    if (postCommand) {
+        logger.info('Running post command...');
+        const command = process.platform === 'win32' ? process.env.COMSPEC || 'cmd.exe' : process.env.SHELL || '/bin/sh';
+        const args = process.platform === 'win32' ? ['/d', '/s', '/c'] : ['-c'];
+        args.push(postCommand);
+        logger.info('Running command', { command, args });
+        const result = (0, child_process_1.spawnSync)(command, args, {
+            stdio: 'inherit',
+            cwd: workspacePath,
+            env: (0, action_kit_1.newCleanNodeEnv)(),
+        });
+        logger.verbose?.('Command result', { result });
+        if (result.status !== 0) {
+            throw new Error(`Post command failed with status ${result.status}`);
         }
     }
 });
